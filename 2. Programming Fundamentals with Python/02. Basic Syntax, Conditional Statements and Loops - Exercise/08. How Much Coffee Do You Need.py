66abcd292fe952f4ce6569b71a1b475f7b9300f9
PMM1313 <peter_mitanov@yahoo.com>
2024-05-18 00:38:45 +0300

    solved

--- 2. Programming Fundamentals with Python/02. Basic Syntax, Conditional Statements and Loops - Exercise/08. How Much Coffee Do You Need.py
+++ 2. Programming Fundamentals with Python/02. Basic Syntax, Conditional Statements and Loops - Exercise/08. How Much Coffee Do You Need.py
@@ -1,1 +1,18 @@
-aaaa
+
+number_of_coffees = 0
+
+while True:
+    command = input()
+
+    if command == 'END':
+        break
+    elif command == 'coding' or command == 'dog' or command == 'cat' or command == 'movie':
+        number_of_coffees += 1
+    elif command == 'CODING' or command == 'DOG' or command == 'CAT' or command == 'MOVIE':
+        number_of_coffees += 2
+
+if number_of_coffees > 5:
+    print('You need extra sleep')
+else:
+    print(number_of_coffees)
+
