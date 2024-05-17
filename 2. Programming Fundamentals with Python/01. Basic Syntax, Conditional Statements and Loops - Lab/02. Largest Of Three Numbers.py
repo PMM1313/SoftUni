0b1a2e1dcb8dd537d74b8b9a5ee2c4eb7798eaea
PMM1313 <peter_mitanov@yahoo.com>
2024-05-17 12:24:08 +0300

    01.Number Definer solved

--- 2. Programming Fundamentals with Python/01. Basic Syntax, Conditional Statements and Loops - Lab/02. Largest Of Three Numbers.py
+++ 2. Programming Fundamentals with Python/01. Basic Syntax, Conditional Statements and Loops - Lab/02. Largest Of Three Numbers.py
@@ -1,5 +1,11 @@
 
-a = 5
-b = 10
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
 
-if not a > b:
+if num1 >= num2 and num1 >= num3:
+    print(num1)
+elif num2 >= num1 and num2 >= num3:
+    print(num2)
+else:
+    print(num3)
