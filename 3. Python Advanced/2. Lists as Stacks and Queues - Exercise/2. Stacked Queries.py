2574531fdb4a28fd32375019ec9ca659533af3db
PMM1313 <peter_mitanov@yahoo.com>
2025-01-10 21:29:40 +0200

    solved

--- 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/2. Stacked Queries.py
+++ 3. Python Advanced/2. Lists as Stacks and Queues - Exercise/2. Stacked Queries.py
@@ -3,25 +3,19 @@ number = int(input())
 stack = []
 iterations = 0
 
+functions = {
+    "1": lambda x: stack.append(int(x)),
+    "2": lambda: stack.pop() if stack else None,
+    "3": lambda: print(max(stack) if stack else None),
+    "4": lambda: print(min(stack) if stack else None)
+}
+
+
 while iterations < number:
+
     command = input()
-    iterations += 1
-
-    if command.startswith('1'):  # Push operation
-        _, num = command.split()
-        stack.append(int(num))
-
-    elif command == '2':  # Pop operation
-        if stack:
-            stack.pop()
-
-    elif command == '3':  # Print maximum
-        if stack:
-            print(max(stack))
-
-    elif command == '4':  # Print minimum
-        if stack:
-            print(min(stack))
+    functions[command[0]](*command[1:])
 
 # Print stack from top to bottom in the required format
-print(", ".join(map(str, reversed(stack))))
+print(*reversed(stack), sep=", ")
+
