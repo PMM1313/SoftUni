ebbe05ff0e29cee9eb647098c77b929c2d8274aa
PMM1313 <peter_mitanov@yahoo.com>
2025-01-15 23:55:08 +0200

    solved

--- 3. Python Advanced/5. Stacks, Queues, Tuples and Sets - Exercise/6. Paint Colors.py
+++ 3. Python Advanced/5. Stacks, Queues, Tuples and Sets - Exercise/6. Paint Colors.py
@@ -1,54 +1,36 @@
 from collections import deque
 
-presents = {
-    150: 'Doll',
-    250: 'Wooden train',
-    300: 'Teddy bear',
-    400: 'Bicycle'
+colors_strings = deque(input().split())
+
+main_colors = ["red", "yellow", "blue"]
+
+secondary_colors = {
+    "orange": ["red", "yellow"],
+    "purple": ["red", "blue"],
+    "green": ["blue", "yellow"]
 }
 
-materials = [int(x) for x in input().split()]
-magic_values = deque(int(x) for x in input().split())
-crafted_presents = dict()
+collected_colors = []
 
-while True:
-    if not materials or not magic_values:
-        break
+while colors_strings:
+    first_str = colors_strings.popleft()
+    last_str = colors_strings.pop() if colors_strings else ""
+    for color in (first_str + last_str, last_str + first_str):
+        if color in main_colors or color in secondary_colors:
+            collected_colors.append(color)
+            break
 
-    current_material = materials.pop()
-    current_magic = magic_values.popleft()
-    total_magic = current_material * current_magic
+    else:
+        if len(first_str) > 1:
+            colors_strings.insert(len(colors_strings) // 2, first_str[:-1])
+        if len(last_str) > 1:
+            colors_strings.insert(len(colors_strings) // 2, last_str[:-1])
 
-    if total_magic in presents.keys():
-        if presents[total_magic] not in crafted_presents.keys():
-            crafted_presents[presents[total_magic]] = 1
-            continue
-        crafted_presents[presents[total_magic]] += 1
-        continue
+for color in collected_colors:
+    if color in secondary_colors:
+        for c in secondary_colors[color]:
+            if c not in collected_colors:
+                collected_colors.remove(color)
+                break
 
-    if total_magic < 0:
-        materials.append(current_material + current_magic)
-
-    elif total_magic > 0:
-        current_material += 15
-        materials.append(current_material)
-
-    elif total_magic == 0:
-        if current_material != 0:
-            materials.append(current_material)
-        if current_magic != 0:
-            magic_values.appendleft(current_magic)
-
-if ('Doll' in crafted_presents and 'Wooden train' in crafted_presents) or \
-        ('Teddy bear' in crafted_presents and 'Bicycle' in crafted_presents):
-    print('The presents are crafted! Merry Christmas!')
-else:
-    print('No presents this Christmas!')
-
-if materials:
-    print(f"Materials left: {', '.join(str(x) for x in materials[::-1])}")
-if magic_values:
-    print(f"Magic left: {', '.join(str(x) for x in magic_values)}")
-
-for key, value in sorted(crafted_presents.items(), key=lambda x: x[0]):
-    print(f"{key}: {value}")
+print(collected_colors)
