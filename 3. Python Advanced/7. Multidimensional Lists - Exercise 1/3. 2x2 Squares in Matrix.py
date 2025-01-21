37ed6790dda5ecbd18218f34d306f191cef57dcf
PMM1313 <peter_mitanov@yahoo.com>
2025-01-21 17:38:09 +0200

    solved

---
+++ 3. Python Advanced/7. Multidimensional Lists - Exercise 1/3. 2x2 Squares in Matrix.py
@@ -0,0 +1,15 @@
+rows, column = [int(x) for x in input().split()]
+
+matrix = [input().split() for _ in range(rows)]
+
+count = 0
+
+for row in range(rows -1):
+    for col in range(column -1):
+        if (matrix[row][col] == matrix[row][col + 1]
+                == matrix[row + 1][col]
+                == matrix[row + 1][col + 1]
+                == matrix[row + 1][col + 1]):
+            count += 1
+
+print(count)
