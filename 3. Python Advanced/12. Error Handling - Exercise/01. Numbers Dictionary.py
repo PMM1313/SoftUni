064de439196a34ccd3db4d50102db7a88457204a
PMM1313 <peter_mitanov@yahoo.com>
2025-01-27 19:20:23 +0200

    solved

--- 3. Python Advanced/12. Error Handling - Exercise/01. Numbers Dictionary.py
+++ 3. Python Advanced/12. Error Handling - Exercise/01. Numbers Dictionary.py
@@ -4,18 +4,32 @@ line = input()
 
 while line != "Search":
     number_as_string = line
-    number = int(input())
-    numbers_dictionary[number_as_string] = number
+    try:
+        number = int(input())
+    except ValueError:
+        print("The variable number must be an integer")
+    else:
+        numbers_dictionary[number_as_string] = number
+    line = input()
 
 line = input()
 while line != "Remove":
     searched = line
-    print(numbers_dictionary[searched])
+    try:
+        print(numbers_dictionary[searched])
+    except KeyError:
+        print("Number does not exist in dictionary")
+    line = input()
 
 line = input()
 
 while line != "End":
     searched = line
-    del numbers_dictionary[searched]
+    try:
+        del numbers_dictionary[searched]
+    except KeyError:
+        print("Number does not exist in dictionary")
+    line = input()
+
 
 print(numbers_dictionary)
