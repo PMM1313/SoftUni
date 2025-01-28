e0efc0a4f9cbdbeb8234e5b80a92b0978b22b42b
PMM1313 <peter_mitanov@yahoo.com>
2025-01-28 09:29:44 +0200

    solved

--- 3. Python Advanced/12. Error Handling - Exercise/04. Rotate Matrix.py
+++ 3. Python Advanced/12. Error Handling - Exercise/04. Rotate Matrix.py
@@ -1,3 +1,10 @@
+class MatrixContentError(Exception):
+    pass
+
+class MatrixSizeError(Exception):
+    pass
+
+
 def rotate_matrix(matrix):
     matrix_length = len(matrix)
 
@@ -14,7 +21,17 @@ while True:
 
     if not line:
         break
+
+    for el in line:
+        if not el.isdigit():
+            raise MatrixContentError("The matrix must consist of only integers")
+
     mtrx.append(line)
+
+
+for row in mtrx:
+    if len(row) != len(mtrx):
+        raise MatrixSizeError("The size of the matrix is not a perfect square")
 
 rotate_matrix(mtrx)
 
