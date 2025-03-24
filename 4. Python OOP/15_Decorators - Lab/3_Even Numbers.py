df901013fe86c860fdc3a5079b1a0617ffec61d2
PMM1313 <peter_mitanov@yahoo.com>
2025-03-24 16:10:55 +0200

    solved

---
+++ 4. Python OOP/15_Decorators - Lab/3_Even Numbers.py
@@ -0,0 +1,10 @@
+def even_numbers(function):
+    def wrapper(*args, **kwargs):
+        result = function(*args, **kwargs)
+        return [el for el in result if el % 2 == 0]
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
---
+++ 4. Python OOP/15_Decorators - Lab/4_Multiply.py
@@ -0,0 +1,13 @@
+
+def multiply(number):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result * number
+        return wrapper
+    return decorator
+
+
+@multiply(3)
+def add_ten(number):
+    return number + 10
